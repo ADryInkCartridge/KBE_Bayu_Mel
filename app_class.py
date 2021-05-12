95c2799b3e9993097ab20e74e1ff4cd77e9245b8
Bayu Adjie Sidharta <31591861+ADryInkCartridge@users.noreply.github.com>
2021-05-12 16:59:44 +0700

    first commit

---
+++ app_class.py
@@ -0,0 +1,9 @@
+import pygame
+from settings import *
+
+pygame.init()
+vec = pygame.math.Vector2
+
+class App:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
