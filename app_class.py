c0519e7b9031f790c3147be5aae824022a588b29
ADryInkCartridge <bayuaaas@gmail.com>
2021-06-01 05:04:18 +0700

    Vid 3

--- app_class.py
+++ app_class.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from settings import *
 
 pygame.init()
@@ -7,3 +8,64 @@ vec = pygame.math.Vector2
 class App:
     def __init__(self):
         self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.state = 'start'
+    
+    def run(self):
+        while self.running == True:
+            if self.state == 'start':
+                self.start_events()
+                self.start_update()
+                self.start_draw()
+            elif self.state == 'playing':
+                self.play_events()
+                self.play_update()
+                self.play_draw()
+            else: 
+                self.running = False
+            self.clock.tick(FPS)
+        pygame.quit()
+        sys.exit()
+
+    def draw_text(self, words, screen,pos, size, color, font_name, centered = False):
+        font = pygame.font.SysFont(font_name,size)
+        text = font.render(words, False, color)
+        text_size = text.get_size()
+        if centered:
+            pos[0] -= text_size[0]/2
+            pos[1] -= text_size[1]/2
+        screen.blit(text,pos)
+
+    def start_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.state = 'playing'
+            
+    
+    def start_update(self):
+        pass
+
+    def start_draw(self):
+        self.screen.fill(BLACK)
+        self.draw_text('Press Space to Play',self.screen,[WIDTH/2,HEIGHT/2],START_TEXT_SIZE,(170,132,58),START_FONT, centered=True)
+        self.draw_text('Single Player only',self.screen,[WIDTH/2,HEIGHT/2+50],START_TEXT_SIZE,(33,137,156),START_FONT, centered=True)
+        self.draw_text('HIGH SCORE',self.screen,[5,0],START_TEXT_SIZE,(255,255,255),START_FONT)
+        pygame.display.update()
+
+    def play_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN and event.type == pygame.K_SPACE:
+                self.state = 'playing'
+            
+    
+    def play_update(self):
+        pass
+
+    def play_draw(self):
+        self.screen.fill(RED)
+        pygame.display.update()
